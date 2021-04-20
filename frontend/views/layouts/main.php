8bf9bb05450e85c9406a1e595acd3414194bff7b
Ramprasad <ramprasad@krititech.in>
2021-04-20 18:01:21 +0530

    Chat Bot Added

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -57,6 +57,19 @@ $this->registerJs("
         
 ?>
 <?php $this->endBody() ?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/607ec80c5eb20e09cf34cbcd/1f3njqtei';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
 <?php $this->endPage() ?>
