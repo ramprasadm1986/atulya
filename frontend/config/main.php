3dbe83f2f5504ebe172cf95ea9d24eeb092fb24c
Ramprasad <ramprasad@krititech.in>
2021-05-28 07:01:43 +0000

    server changes added

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -12,6 +12,7 @@ return [
     'basePath' => dirname(__DIR__),
     'homeUrl' => Yii::getAlias('@frontendUrl'),
     'controllerNamespace' => 'frontend\controllers',
+    'bootstrap'  => ['assetsAutoCompress'],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
@@ -52,6 +53,49 @@ return [
                 'category/<slug>' => 'category/index',
                 'cms-page/<slug>' => 'cms-page/index',
                 'product/<slug>' =>  'product/index',
+            ],
+        ],
+        'assetsAutoCompress' => [
+            'class'   => '\skeeks\yii2\assetsAuto\AssetsAutoCompressComponent',
+            'enabled' => true,
+
+            'readFileTimeout' => 3,           //Time in seconds for reading each asset file
+
+            'jsCompress'                => true,        //Enable minification js in html code
+            'jsCompressFlaggedComments' => true,        //Cut comments during processing js
+
+            'cssCompress' => true,        //Enable minification css in html code
+            
+            'cssFileCompile'        => true,        //Turning association css files
+            'cssFileCompileByGroups' => false,       //Enables the compilation of files in groups rather than in a single file. Works only when the $cssFileCompile option is enabled
+            'cssFileRemouteCompile' => false,       //Trying to get css files to which the specified path as the remote file, skchat him to her.
+            'cssFileCompress'       => true,        //Enable compression and processing before being stored in the css file
+            'cssFileBottom'         => false,       //Moving down the page css files
+            'cssFileBottomLoadOnJs' => false,       //Transfer css file down the page and uploading them using js
+
+            'jsFileCompile'                 => true,        //Turning association js files
+            'jsFileCompileByGroups'         => false,        //Enables the compilation of files in groups rather than in a single file. Works only when the $jsFileCompile option is enabled
+            'jsFileRemouteCompile'          => false,       //Trying to get a js files to which the specified path as the remote file, skchat him to her.
+            'jsFileCompress'                => true,        //Enable compression and processing js before saving a file
+            'jsFileCompressFlaggedComments' => true,        //Cut comments during processing js
+
+            'noIncludeJsFilesOnPjax' => true,        //Do not connect the js files when all pjax requests when all pjax requests when enabled jsFileCompile
+            'noIncludeCssFilesOnPjax' => true,        //Do not connect the css files when all pjax requests when all pjax requests when enabled cssFileCompile
+
+            'htmlFormatter' => [
+                //Enable compression html
+                'class'         => 'skeeks\yii2\assetsAuto\formatters\html\TylerHtmlCompressor',
+                'extra'         => true,       //use more compact algorithm
+                'noComments'    => true,        //cut all the html comments
+                'maxNumberRows' => 50000,       //The maximum number of rows that the formatter runs on
+
+                //or
+
+                //'class' => 'skeeks\yii2\assetsAuto\formatters\html\MrclayHtmlCompressor',
+
+                //or any other your handler implements skeeks\yii2\assetsAuto\IFormatter interface
+
+                //or false
             ],
         ],
         
